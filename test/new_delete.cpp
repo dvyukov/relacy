96c357a9e9473d670b8a9d6682178fe405d2c6d1
Chris Cotter <ccotter14@bloomberg.net>
2024-09-13 22:59:18 -0400

    Fix memory related bugs
     * Use debug_info for data members instead of debug_info_param
       (which is a reference). Caught by ASAN.
     * Supply C++14 overloads for operator delete, which may be
       called by the standard library. Without these overloads, some
       deallocations are not intercepted by relacy, and can lead to
       malloc/free mismatches.

---
+++ test/new_delete.cpp
@@ -0,0 +1,23 @@
+#include "../../relacy/relacy_std.hpp"
+#include "../../relacy/relacy_cli.hpp"
+
+#include <vector>
+
+struct new_delete_tests : rl::test_suite<new_delete_tests, 1>
+{
+    void thread(unsigned index)
+    {
+        std::vector<int> vs;
+        vs.push_back(1);
+        vs.push_back(2);
+        vs.push_back(3);
+    }
+};
+
+int main()
+{
+    rl::test_params p;
+    p.iteration_count = 10;
+    rl::simulate<new_delete_tests>(p);
+    return 0;
+}
